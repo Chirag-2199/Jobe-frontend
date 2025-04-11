f7ebd8c4d23b10eacb39442e14cd5999636c5ef8
Chirag Kumar <147901733+Chirag-2199@users.noreply.github.com>
2025-04-11 19:55:54 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://implicit-briana-chiragkumar2199-99c713f2.koyeb.app/",
+    baseURL: "https://implicit-briana-chiragkumar2199-99c713f2.koyeb.app/api",
 });
 
 export default api;
